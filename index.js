4b3495beb2fafefb8eb8a99544af6a5957faa3f9
Nishaojha <its.nishaojha@gmail.com>
2023-03-19 21:03:16 +0530

    added view for index.ejs

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const { urlencoded } = require('express');
 const express = require ('express');
 const app = express()
 const port = 8080;
-const db = require('./Config/db')
+const db = require('./Config/db');
+const ShortUrl = require('./models/shortUrl');
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/', async(req, res)=>{
+    const shortUrls = await ShortUrl.findOne({})
+    res.render('index',{shortUrls:shortUrls})
+})
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running this server: ${err}`)
